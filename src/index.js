53fee56bb0940f5121e7d37eb84b02c637280185
Nastya Severina <Severina.Nastya1992@yandex.by>
2022-06-30 22:31:48 +0300

    docs: add js solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    const reverseString = n.toString();
+    const reverseArrow = reverseString.split('');
+    if (reverseArrow[0] === '-') {
+      let newResult = reverseArrow.shift()
+      return reverseArrow.reverse().join('');
+    } else {
+      return reverseArrow.reverse().join('');
+    };
 }
